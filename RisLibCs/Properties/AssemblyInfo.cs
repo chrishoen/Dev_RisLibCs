ee5e54da35f4811e926ac7549a02b0c79baea432
chrishoen <chris123hoen@gmail.com>
2019-03-30 11:56:46 -0400

    renamed assembly RisLib to RisLibCs

--- RisLibCs/Properties/AssemblyInfo.cs
+++ RisLibCs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RisLib")]
+[assembly: AssemblyTitle("RisLibCs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RisLib")]
+[assembly: AssemblyProduct("RisLibCs")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
